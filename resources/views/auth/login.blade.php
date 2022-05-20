db53f47470be86bb53240e102ab37cba3f318ebd
robanu-dk <85453277+robanu-dk@users.noreply.github.com>
2022-05-20 20:06:16 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="namaTim" class="col-md-4 col-form-label text-md-end">{{ __('Nama Tim') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="namaTim" type="string" class="form-control @error('namaTim') is-invalid @enderror" name="namaTim" value="{{ old('namaTim') }}" required autocomplete="namaTim" autofocus>
 
-                                @error('email')
+                                @error('namaTim')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
